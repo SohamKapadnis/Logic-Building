030b85b2fe7b0b99aac5c671abd9f515a9fc780a
Soham Kapadnis <sohamkapadnis2006@gmail.com>
2026-06-24 12:30:45 +0530

    New files added

--- Assignment31/program5.c
+++ Assignment31/program5.c
@@ -1,48 +1,50 @@
-// Write a program which accept string from user and check whether it contains vowels in it or not
-// Input : "MarvellouS" 
-// Output : TRUE
+// Write a program which accept string from user and display it inn reverse order
+// Input : "MArvellouS"
+// Output : MuollevrAM
 
 #include<stdio.h>
 
-typedef int BOOL;
-
-#define TRUE 1
-#define FALSE 0
-
-BOOL ChkVowel(char *str)
+void Reverse(char *str)
 {
+    char *start = NULL;
+    char *end = NULL;       // 100 200 300 400 500 600
+    char temp = '\0';       // start            end    
+
+    start = str;
+
     while (*str != '\0')
     {
-        if(
-             *str == 'A' || *str == 'E' || *str == 'I' || *str == 'O' || *str == 'U' || 
-             *str == 'a' || *str == 'e' || *str == 'i' || *str == 'o' || *str == 'u' 
-          )
-        {
-            return TRUE;
-        }
         str++;
     }
-    return FALSE;
+    str--;
+
+    end = str;
+
+    while (start < end)
+    {
+        temp = *start;
+        *start = *end;
+        *end = temp;
+
+        start++;
+        end--;
+    }
+    
+    
+
+    
 }
 
 int main()
 {
     char arr[20];
-    int bRet = FALSE;
 
     printf("Enter String : ");
     scanf("%[^\n]c",arr);
 
-    bRet = ChkVowel(arr);
+    Reverse(arr);
 
-    if(bRet == TRUE)
-    {
-        printf("It Contains Vowels");
-    }
-    else
-    {
-        printf("There is NO vowels");
-    }
+    printf("%s",arr);
 
     return 0;
 }
